eae7af8b4937d260c195b63226661e3dcaf974ce
Sergey Popov <poserg@gmail.com>
2018-07-24 16:23:39 +0300

    Implement servlet

---
+++ webapp/src/main/java/com/github/poserg/liquibase/servlet/MyLiquibaseServletListener.java
@@ -0,0 +1,7 @@
+package com.github.poserg.liquibase.servlet;
+
+import liquibase.integration.servlet.LiquibaseServletListener;
+
+public class MyLiquibaseServletListener extends LiquibaseServletListener {
+
+}
